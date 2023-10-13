f30784c184c1f3bdcecde87a0c4c34518187b481
MrLuje <ogmniluje@gmail.com>
2023-10-13 18:14:02 +0000

    js + fsharp

--- build/generateGrammar.js
+++ build/generateGrammar.js
@@ -4,44 +4,92 @@ const fs = require('fs');
 const path = require('path');
 const { languages } = require('./languages');
 
-const targetScopes = [
-'source.fsharp',
-'source.fsharp.fsi',
-'source.fsharp.fsx',
-'source.fsharp.fsl'
+const mainLanguages = [
+    {
+        name: 'fsharp',
+        targetScopes: [
+            'source.fsharp',
+            'source.fsharp.fsi',
+            'source.fsharp.fsx',
+            'source.fsharp.fsl'
+        ],
+        basicGrammarPattern: {
+            begin: x => `(?i)(\\*\\s*\\b(?:${x})\\b\\s*\\*\\))\\s*(""")`,
+            beginCaptures: {
+                1: { name: 'comment.block.fsharp' },
+                2: { name: 'punctuation.definition.string.begin.fsharp' }
+            },
+            end: `(?=""")`
+        },
+        basicGrammar: {
+            begin: x => `(?i)(\\()(?=(\\*\\s*\\b(?:${x})\\b\\s*\\*\\))\\s*""")`,
+            beginCaptures: {
+                1: { name: 'comment.block.fsharp' }
+            },
+            end: '(""")',
+            endCaptures: {
+                0: { name: 'string.quoted.double.fsharp' },
+                1: { name: 'punctuation.definition.string.end.fsharp' }
+            },
+        }
+    },
+    {
+        name: 'javascript',
+        targetScopes: ['source.js', 'source.jsx', 'source.js.jsx', 'source.ts', 'source.tsx'],
+        basicGrammarPattern: {
+            begin: x => `(?i)(\\*\\s*\\b(?:${x})\\b\\s*\\*/)\\s*(\`)`,
+            beginCaptures: {
+                1: { name: 'comment.block.ts' },
+                2: { name: 'punctuation.definition.string.template.begin.js' }
+            },
+            end: '(?=`)'
+        },
+        basicGrammar: {
+            begin: x => `(?i)(/)(?=(\\*\\s*\\b(?:${x})\\b\\s*\\*/)\\s*\`)`,
+            beginCaptures: {
+                1: { name: 'comment.block.ts' }
+            },
+            end: '(`)',
+            endCaptures: {
+                0: { name: 'string.js' },
+                1: { name: 'punctuation.definition.string.template.end.js' }
+            },
+        }
+    }
 ]
 
-const basicGrammarTemplate = {
-    "fileTypes": [],
-    "injectionSelector": getBasicGrammarInjectionSelector(),
-    "patterns": [],
-    "scopeName": "inline.template-tagged-languages"
+function basicGrammarTemplate(mainLanguage) {
+    return {
+        "fileTypes": [],
+        "injectionSelector": getBasicGrammarInjectionSelector(mainLanguage),
+        "patterns": [],
+        "scopeName": `inline.${mainLanguage.name}.template-tagged-languages`
+    }
 };
 
-const reinjectGrammarTemplate = {
-    "fileTypes": [],
-    "injectionSelector": getReinjectGrammarInjectionSelector(),
-    "patterns": [
-        {
-            "include": "source.ts#template-substitution-element"
-        }
-    ],
-    "scopeName": "inline.template-tagged-languages.reinjection"
+function reinjectGrammarTemplate(mainLanguage) {
+    return {
+        "fileTypes": [],
+        "injectionSelector": getReinjectGrammarInjectionSelector(mainLanguage),
+        "patterns": [
+            {
+                "include": "source.ts#template-substitution-element"
+            }
+        ],
+        "scopeName": `inline.${mainLanguage.name}.template-tagged-languages.reinjection`
+    }
 };
 
-const getBasicGrammarPattern = (language) => {
+const getBasicGrammarPattern = (mainLanguage, language) => {
     const sources = Array.isArray(language.source) ? language.source : [language.source];
     return {
         name: `string.js.taggedTemplate.commentTaggedTemplate.${language.name}`,
         contentName: `meta.embedded.block.${language.name}`,
 
         // The leading '/' was consumed by outer rule
-        begin: `(?i)(\\*\\s*\\b(?:${language.identifiers.map(escapeRegExp).join('|')})\\b\\s*\\*\\))\\s*(""")`,
-        beginCaptures: {
-            1: { name: 'comment.block.fsharp' },
-            2: { name: 'punctuation.definition.string.begin.fsharp' }
-        },
-        end: `(?=""")`,
+        begin: mainLanguage.basicGrammarPattern.begin(language.identifiers.map(escapeRegExp).join('|')),
+        beginCaptures: mainLanguage.basicGrammarPattern.beginCaptures,
+        end: mainLanguage.basicGrammarPattern.end,
         patterns: [
             ...sources.map(source => ({ 'include': source })),
             // When a language grammar is not installed, insert a phony pattern
@@ -53,11 +101,11 @@ const getBasicGrammarPattern = (language) => {
     };
 };
 
-const getBasicGrammar = () => {
-    const basicGrammar = basicGrammarTemplate;
+const getBasicGrammar = mainLanguage => {
+    const basicGrammar = basicGrammarTemplate(mainLanguage);
 
     basicGrammar.repository = languages.reduce((repository, language) => {
-        repository[getRepositoryName(language)] = getBasicGrammarPattern(language);
+        repository[getRepositoryName(language)] = getBasicGrammarPattern(mainLanguage, language);
         return repository;
     }, {});
 
@@ -67,15 +115,10 @@ const getBasicGrammar = () => {
         // @ts-ignore
         {
             // Match entire language comment identifier but only consume '/'
-            begin: `(?i)(\\()(?=(\\*\\s*\\b(?:${allLanguageIdentifiers.map(escapeRegExp).join('|')})\\b\\s*\\*\\))\\s*""")`,
-            beginCaptures: {
-                1: { name: 'comment.block.fsharp' }
-            },
-            end: '(""")',
-            endCaptures: {
-                0: { name: 'string.quoted.double.fsharp' },
-                1: { name: 'punctuation.definition.string.end.fsharp' }
-            },
+            begin: mainLanguage.basicGrammar.begin(allLanguageIdentifiers.map(escapeRegExp).join('|')),
+            beginCaptures: mainLanguage.basicGrammar.beginCaptures,
+            end: mainLanguage.basicGrammar.end,
+            endCaptures: mainLanguage.basicGrammar.endCaptures,
             patterns: languages.map(language => ({ include: '#' + getRepositoryName(language) }))
         }
     ]
@@ -87,14 +130,14 @@ function getRepositoryName(langauge) {
     return 'commentTaggedTemplate-' + langauge.name;
 }
 
-function getBasicGrammarInjectionSelector() {
-    return targetScopes
+function getBasicGrammarInjectionSelector(mainLanguage) {
+    return mainLanguage.targetScopes
         .map(scope => `L:${scope} -comment -(string - meta.embedded)`)
         .join(', ');
 }
 
-function getReinjectGrammarInjectionSelector() {
-    return targetScopes
+function getReinjectGrammarInjectionSelector(mainLanguage) {
+    return mainLanguage.targetScopes
         .map(scope => {
             const embeddedScopeSelectors = languages.map(language => `meta.embedded.block.${language.name}`);
             return `L:${scope} (${embeddedScopeSelectors.join(', ')})`
@@ -112,10 +155,12 @@ function writeJson(outFile, json) {
 
 exports.updateGrammars = () => {
     const outDir = path.join(__dirname, '..', 'syntaxes');
-    writeJson(path.join(outDir, 'grammar.json'), getBasicGrammar());
+    mainLanguages.forEach(mainLanguage => {
+        writeJson(path.join(outDir, `grammar-${mainLanguage.name}.json`), getBasicGrammar(mainLanguage));
+        writeJson(
+            path.join(outDir, `reinject-grammar-${mainLanguage.name}.json`),
+            reinjectGrammarTemplate(mainLanguage));
+    });
 
-    writeJson(
-        path.join(outDir, 'reinject-grammar.json'),
-        reinjectGrammarTemplate);
 };
 
